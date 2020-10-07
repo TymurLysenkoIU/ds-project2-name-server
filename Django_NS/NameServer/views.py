0dc56fc31400e648d495271dd6e781c54d27783d
Insaf <safinsaft@gmail.com>
2020-10-07 18:02:38 +0300

    Django server

---
+++ Django_NS/NameServer/views.py
@@ -0,0 +1,37 @@
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from urllib import parse
+from .parse_request import Request_parser
+
+@csrf_exempt
+def send_request(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+
+    if request.method == 'GET':
+        pyDict = dict(request.GET.lists())
+        pyDict = {int(key):pyDict[key][0] for key in pyDict}
+        array = [0 for i in range(len(pyDict))]
+        for key, val in pyDict.items():
+            array[key] = val
+        parser = Request_parser()
+        answer = parser.parse(array)
+        if pyDict[0] != 'read':
+            return HttpResponse(str(answer), status=200)
+        else:
+            return HttpResponse(answer, enctype="multipart/form-data",
+                                status=200)
+
+    elif request.method == 'POST':
+        print(request)
+        url = request.get_full_path()
+        args = dict(parse.parse_qsl(parse.urlsplit(url).query))
+        pyDict = {int(key): args[key] for key in args}
+        array = [0 for i in range(len(pyDict))]
+        for key, val in pyDict.items():
+            array[key] = val
+        file = request.FILES['file'].read()
+        parser = Request_parser()
+        answer = parser.parse(array, file)
+        return HttpResponse(str(answer), status=200)
